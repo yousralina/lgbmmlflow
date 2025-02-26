ebc5e3189ebb3bedd8cb0c04902fe00230f9235b
yousralina <113200232+yousralina@users.noreply.github.com>
2025-02-26 20:57:21 +0100

    Initial commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = FastAPI()
 
 # Charger le modèle via MLflow
-model_uri = 'runs:/15a09831c7cc44fe906abf30f8b39a22/LGBM_Undersampling_Pipeline'
+model_uri = "file:///C:/Users/yosra/mlartifacts/970618126747358610/15a09831c7cc44fe906abf30f8b39a22/artifacts/mon_projet_api/model/LGBM_Undersampling_Pipeline"
 try:
     model = mlflow.pyfunc.load_model(model_uri)
 
